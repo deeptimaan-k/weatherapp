87648c5edd6cb3ccad0b4af8fa1423435a8c045c
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-11-16 12:51:39 +0530

    waterqualityDesignComplete
    use of metrial UI UX in this page

--- src/WaterqualityCalculator.js
+++ src/WaterqualityCalculator.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Container, TextField, Button,Grid} from '@mui/material';
+import videoBackground from './assets/common_background.mp4';
 
 const WaterQualityIndexCalculator = () => {
   const [temperature, setTemperature] = useState('');
@@ -20,35 +22,122 @@ const WaterQualityIndexCalculator = () => {
   };
 
   return (
+    <>
+     <style>
+        {`
+          .MuiOutlinedInput-notchedOutline {
+            border-color: white !important;
+          }
+
+          .MuiInputLabel-root {
+            color: #AAA6C3 !important;
+          }
+
+          .MuiInputBase-input {
+            color: white !important;
+          }
+        `}
+      </style>
+    <video
+    autoPlay
+    loop
+    muted
+    style={{
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      opacity: 0.6,
+      objectFit: 'cover',
+      zIndex: -1, // Set a negative z-index to ensure the video is in the background
+    }}
+  >
+    <source src={videoBackground} type="video/mp4" />
+  </video>
+  <Container
+      sx={{
+        maxWidth: { xs: '95%', sm: '80%', md: '1100px' },
+        width: '100%',
+        height: '100%',
+        margin: '0 auto',
+        padding: '1rem 0 3rem',
+        marginBottom: '1rem',
+        borderRadius: {
+          xs: 'none',
+          sm: '0 0 1rem 1rem',
+        },
+        boxShadow: {
+          xs: 'none',
+          sm: 'rgba(0,0,0, 0.5) 0px 10px 15px -3px, rgba(0,0,0, 0.5) 0px 4px 6px -2px',
+        },
+      }}
+    >
     <div>
-      <h1>Water Quality Index Calculator</h1>
-      <div>
-        <label>Temperature (°C):</label>
-        <input type="number" value={temperature} onChange={(e) => setTemperature(e.target.value)} />
-      </div>
-      <div>
-        <label>Biological Oxygen Demand (BOD) (mg/L):</label>
-        <input type="number" value={bod} onChange={(e) => setBod(e.target.value)} />
-      </div>
-      <div>
-        <label>Total Suspended Solids (TSS) (mg/L):</label>
-        <input type="number" value={tss} onChange={(e) => setTss(e.target.value)} />
-      </div>
-      <div>
-        <label>Dissolved Oxygen (DO) (mg/L):</label>
-        <input type="number" value={dissolvedOxygen} onChange={(e) => setDissolvedOxygen(e.target.value)} />
-      </div>
-      <div>
-        <label>Conductivity (μS/cm):</label>
-        <input type="number" value={conductivity} onChange={(e) => setConductivity(e.target.value)} />
-      </div>
-      <button onClick={calculateWaterQualityIndex}>Calculate</button>
+    <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <h1 style={{ color:'white',fontSize:'30px'}}>Water Quality Index Calculator</h1>
+              </Grid>
+            <Grid container spacing={2} justifyContent="center" alignItems="center">
+            <Grid item xs={12}>
+            <TextField
+                  label="Temperature (°C)"
+                  type="number"
+                  fullWidth
+                  value={temperature} onChange={(e) => setTemperature(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                  label="Biological Oxygen Demand (BOD) (mg/L)"
+                  type="number"
+                  value={bod} onChange={(e) => setBod(e.target.value)} 
+                  fullWidth
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                  label="Total Suspended Solids (TSS) (mg/L)"
+                  type="number"
+                  value={tss} onChange={(e) => setTss(e.target.value)}
+                  fullWidth
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                  label="Dissolved Oxygen (DO) (mg/L)"
+                  type="number"
+                  value={dissolvedOxygen} onChange={(e) => setDissolvedOxygen(e.target.value)}
+                  fullWidth
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                  label="Conductivity (μS/cm)"
+                  type="number"
+                  value={conductivity} onChange={(e) => setConductivity(e.target.value)}
+                  fullWidth
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Button type="button" variant="contained" onClick={calculateWaterQualityIndex} style={{ backgroundColor: '#050816', fontSize: '20px', border: '2px solid green', color: 'white' }}>
+                Calculate
+                </Button>
+              </Grid>
+              </Grid>
+      {/* <Button variant="contained" onClick={calculateWaterQualityIndex}>Calculate</Button> */}
       {waterQualityIndex && (
-        <div>
-          <h2>Water Quality Index (ISQA): {waterQualityIndex}</h2>
-        </div>
+        <Grid item xs={12} style={{ textAlign: 'center' }}>
+        <h1 style={{ color:'white',fontSize:'30px'}}>Water Quality Index (ISQA): {waterQualityIndex} </h1>
+      </Grid>
+        // <div>
+        //   <h2>Water Quality Index (ISQA): {waterQualityIndex}</h2>
+        // </div>
       )}
     </div>
+    </Container>
+    </>
   );
 };
 
